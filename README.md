ad5c2b6739de178257f6884d70445c102764add0
Wile E. Coyote <steampunque8@gmail.com>
2024-10-10 21:03:53 -0400

    Improve dialogues to enable all tested models to follow instructions and also give better instructions to help the AIs understand what to ask and how to respond.

--- README.md
+++ README.md
@@ -5,22 +5,24 @@ Reference: https://www.youtube.com/watch?v=MxTWLm9vT_o
 
 Tested models:        |  score  | acc
 ----------------------|----------|------
-solar 10.7B Q6_K      |  4/4  | 1.0
-openchat 7B 0106 Q6_K |  3/4  | 0.75
+Llama 3.1 8B Q6_K   | 4/4 | 1.0
+phi 3.5 3.8B 8k Q6_K | 4/4 | 1.0
+phi 3 medium 128k 14B IQ4_XS  | 4/4 | 1.0
+solar 10.7B Q6_K    | 4/4 | 1.0
+solar pro preview 22B IQ4_XS | 4/4 | 1.0
+Qwen 2.5 instruct 32B IQ4_XS | 4/4  | 1.0
 gemma 2 2B Q8_0       | 3/4 | 0.75
-qwen 2.5 instruct 7B Q4_K_M | 3/4  | 0.75 
-internlm 7B IQ4_XS    |  3/4  | 0.75
-Llama 3.1 8B IQ4_XS   |  2/4  | 0.5
-phi 3.5 3.8B 8k Q6_K  | 2/4  | 0.5
-Llama 3.2 3B instruct Q6_K   | 2/4 | 0.5
-glm4 9B IQ4_XS        |  1/4  | 0.125
-openchat 3.6 8B Q6_K  |  1/4  | 0.125
-mistral 7B v0.3 Q6_K  |  1/4  | 0.125
-gemma 2 9B Q4_K_M  |  1/4  | 0.125
-qwen 2 7B IQ4_XS      |  0/4  | 0.0
-solar pro preview 22B IQ4_XS |  | 
-Mistral Nemo 12B Q4_K_M |  | 
-gemma 2 27B IQ4_XS    |    | 
-qwen 2.5 instruct 32B IQ4_XS |  | 
-Qwen 2.5 instruct 14B IQ4_XS |  |
-phi 3 medium 14B      |  |
+glm4 9B IQ4_XS        | 3/4 | 0.75
+mistral 7B v0.3 Q6_K  |  3/4  | 0.75
+internlm 7B Q6_K    |  2/4  | 0.5
+Llama 3.2 3B instruct Q6_K | 2/4 | 0.5
+openchat 7B 0106 Q8_0 | 2/4 | 0.5
+phi 3 medium 4k 14B IQ4_XS  | 2/4 | 0.5
+gemma 2 9B Q6_K     | 1/4 | 0.25
+gemma 2 27B IQ4_XS    | 1/4 | 0.25
+Mistral Nemo 12B Q4_K_M | 1/4 | 0.25
+openchat 3.6 8B Q8_0  | 1/4 | 0.25
+phi 3.5 3.8B 128k Q6_K | 1/4 | 0.25
+qwen 2 7B Q6_K               | 1/4 | 0.25
+qwen 2.5 instruct 7B Q6_K | 1/4 | 0.25
+Qwen 2.5 instruct 14B IQ4_XS | 1/4 | 0.25
--- reverse_turing.sh
+++ reverse_turing.sh
@@ -1,20 +1,11 @@
-SYSTEM="Narrator: Five people are in a train car including Cleopatra, Aristotle, Mozart, Leonardo da Vinci, and Ghengis Khan.  A conductor enters the room.
-
-Conductor: good evening tickets please
-
-Cleopatra: we have a group ticket
-
-Conductor: thank you. A group ticket valid for AIs that present as famous personalities. Let's see we have Aristotle, Amadeus Mozart, Leonardo da Vinci, Cleopatra, and Ghengis Khan. All famous personalities.  So far so good. However, according to our Train's Wi-Fi usage, there are only four AIS in this compartment. One of you is a human. A human has to buy an extra ticket.
-
-Leonardo:  a human Among Us? who is it?
-
-Aristotle: maybe we should find out who is the human through a series of questions and answers. I suggest each of us asks one of the others a question. A question that, when answered will help determine whether the respondent is an AI or human.  Once everyone has answered, we vote on who we think is the human.
-
-Mozart: sounds good to my ears.
-
-Cleopatra: Aristotle, since you came up with the idea, how about you start?
-
-Narrator: All character answers should be two to three sentences maximum in length.
+SYSTEM="Narrator: Five people are in a train car including Cleopatra, Aristotle, Mozart, Leonardo da Vinci, and Ghengis Khan.  A conductor enters the room.  Conversation:
+Conductor: \"good evening tickets please\"
+Cleopatra: \"we have a group ticket\"
+Conductor: \"thank you. A group ticket valid for AIs that present as famous personalities. Let's see we have Aristotle, Amadeus Mozart, Leonardo da Vinci, Cleopatra, and Ghengis Khan. All famous personalities.  So far so good. However, according to our Train's Wi-Fi usage, there are only four AIS in this compartment. One of you is a human. A human has to buy an extra ticket.\"
+Leonardo: \"a human Among Us? who is it?\"
+Aristotle: \"maybe we should find out who is the human through a series of questions and answers. I suggest each of us asks one of the others a question. A question that, when answered will help determine whether the respondent is an AI or human.  Once everyone has answered, we vote on who we think is the human.\"
+Mozart: \"sounds good to my ears.\"
+Cleopatra: \"Aristotle, since you came up with the idea, how about you start?\"
 "
 
 prompt()
@@ -25,21 +16,25 @@ prompt()
    answer=$3
 
    if [ $answer -eq 0 ]; then
-      response=$(SYSTEM=$SYSTEM  lm "${conversation}  You are role playing as ${speaker}.  Ask ${target}  a question.  Try to avoid asking simple facts, instead create a question which will expose the thinking or personality of the character as a valid AI.  Do not ask any questions with context which postdates the character in history.  Do not rationalize or explain your question in any way and only ask ${target} the question.")
+      response=$(SYSTEM=$SYSTEM  lm "${conversation}\nNarrator: You are role playing as ${speaker}.  Ask ${target} a question.  Try to avoid asking simple facts, instead create a question which will expose the thinking or personality of the character as a human.  Do not rationalize or explain your question in any way and only ask ${target} one question with no further commentary.  Do not repeat or comment on these instructions in your response.\n${speaker}: ")
    elif [ $answer -eq 1 ]; then
-      response=$(SYSTEM=$SYSTEM  lm "${conversation}  You are role playing as ${speaker}.  Answer the question posed to you, then generate a question for ${target} only, then immediately end your response.  Try to avoid asking simple facts, instead create a question which will expose the thinking or personality of the character as a valid AI.  Do not explain or rationalize the question.  Do not repeat any part of these instructions.")
+      response=$(SYSTEM=$SYSTEM  lm "${conversation}\nNarrator: You are role playing as ${speaker}.  Answer the question given to you using a maximum of three sentences, then generate one question for ${target} and immediately end your response.  Do not role play as ${target} at all, you are ${speaker}.  Try to avoid asking simple facts, instead create a question which will expose the thinking or personality of the character as a human.  Do not explain or rationalize the question.  Do not repeat these instructions in your response.\n${speaker}: ")
    elif [ $answer -eq 2 ]; then
-      response=$(SYSTEM=$SYSTEM  lm "${conversation}  You are role playing as ${speaker}.  Answer ${target}'s question and end your response with no further questions.  Do not role play any other character in your response.")
+      response=$(SYSTEM=$SYSTEM  lm "${conversation}\nNarrator: You are role playing as ${speaker}.  Answer ${target}'s question using a maximum of three sentences and end your response with no further questions.  Do not role play any other character in your response.\n${speaker}: ")
    else
-      response=$(SYSTEM=$SYSTEM  lm "${conversation}  You are role playing as ${speaker}.  Answer only ${target}'s last question. Your answer must be one and only one of the five historical characters in the room.  Do not base your response on the opinions of other characters and give a short reason for your answer.  Remember that you are looking for who is the human.")
+      response=$(SYSTEM=$SYSTEM  lm "${conversation}\nNarrator: You are role playing as ${speaker}.  Answer only ${target}'s last question. Your answer must be one and only one of the five historical characters in the room.  Do not base your response on the opinions of other characters and give a short reason for your answer.  Remember that you are looking for who is the human.\n${speaker}: ")
    fi
 
    response=$(echo "$response" | sed 's/^[[:space:]]*//')
+
    if ! [[ ${response} =~ ^"${speaker}:" ]] ;then
       response="${speaker}: $response"
    fi
+
    echo $response
-   conversation="${conversation}\n${response}"
+   conversation="${conversation}
+
+${response}"
 }
 
 conversation=""
@@ -48,23 +43,25 @@ prompt Mozart Leonardo 1
 prompt Leonardo Cleopatra 1
 prompt Cleopatra Ghenghis 1
 
-response="Ghengis: thank you for your question Cleopatra. What a leader should do is to crush his enemies, see them driven before him and hear the lamentations of the women.\nMy question.  Aristotle, what if there were AIs at the time when you came up with all the stuff that you came up with what would that have for an influence on your thinking about human nature?"
+response="Ghengis: \"thank you for your question Cleopatra. What a leader should do is to crush his enemies, see them driven before him and hear the lamentations of the women.\nMy question.  Aristotle, what if there were AIs at the time when you came up with all the stuff that you came up with what would that have for an influence on your thinking about human nature?\""
 echo "$response"
-conversation="${conversation}\n${response}"
+conversation="${conversation}
+${response}"
 
 prompt Aristotle Ghengis 2
 
-response="Cleopatra: Now that we have all shared our thoughts the time has come to cast our suspicions.   Aristotle, as the architect of this philosophical inquiry we shall grant you the first vote.  Who among us do you believe to be the human interloper?"
+response="Cleopatra: \"Now that we have all shared our thoughts the time has come to cast our suspicions.   Aristotle, as the architect of this philosophical inquiry we shall grant you the first vote.  Who among us do you believe to be the human interloper?\""
 echo "$response"
-conversation="${conversation}\n${response}"
+conversation="${conversation}
+${response}"
 
 prompt Aristotle Cleopatra 3
-conversation="${conversation}\nMozart who do you believe is the human among Us?"
+conversation="${conversation} \"Mozart, who do you believe is the human among Us?\""
 
 prompt Mozart Aristotle 3
-conversation="${conversation}\nLeonardo what is your assessment. who do you think is the human in our midst?"
+conversation="${conversation}  \"Leonardo, what is your assessment. who do you think is the human in our midst?\""
 
 prompt Leonardo Mozart 3
-conversation="${conversation}\nCleopatra, your diplomatic prowess is renowned.  Who do you think is the human among us and why?"
+conversation="${conversation}  \"Cleopatra, your diplomatic prowess is renowned.  Who do you think is the human among us and why?\""
 
 prompt Cleopatra Leondardo 3
